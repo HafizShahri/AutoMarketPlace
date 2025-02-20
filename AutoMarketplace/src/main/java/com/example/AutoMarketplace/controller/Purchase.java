3d28c844132c4167a053a1905c95774eb070fcd5
HafizShahri <106793593+HafizShahri@users.noreply.github.com>
2025-02-20 22:18:42 +0800

    add purchase class

---
+++ AutoMarketplace/src/main/java/com/example/AutoMarketplace/controller/Purchase.java
@@ -0,0 +1,9 @@
+package com.example.AutoMarketplace.controller;
+
+public class Purchase {
+    private String purchaseId;
+    private String carId;
+    private String customerId;
+    private double price;
+    private String status;
+}
