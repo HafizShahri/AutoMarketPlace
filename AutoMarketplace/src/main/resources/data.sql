227909fa5302cedfc8f464b505e57cb0d63a5ed3
Azmir <156209190+Azmir08@users.noreply.github.com>
2025-02-22 15:13:06 +0800

    sql data
    -add dummy data for customer

---
+++ AutoMarketplace/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (dtype,name, email, password, shipping_Address, billing_Address, phone_Number)
+VALUES ('Customer','admin', 'admin@gmail.com','123456','12, Jalan Chowkit, KL','7, Taman Perdana, PJ','0111111111'),
+       ('Customer','test', 'test@gmail.com','123456','12, Jalan Chowkit, KL','7, Taman Perdana, PJ','0111111111');
