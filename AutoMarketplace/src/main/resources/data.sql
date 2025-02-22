2a1418a83af3e5f1413873eda181e649a0ea0d81
Azmir <156209190+Azmir08@users.noreply.github.com>
2025-02-22 15:38:45 +0800

    SQL data
    -add dummy data for car

--- AutoMarketplace/src/main/resources/data.sql
+++ AutoMarketplace/src/main/resources/data.sql
@@ -1,3 +1,6 @@
 INSERT INTO users (dtype,name, email, password, shipping_Address, billing_Address, phone_Number)
 VALUES ('Customer','admin', 'admin@gmail.com','123456','12, Jalan Chowkit, KL','7, Taman Perdana, PJ','0111111111'),
        ('Customer','test', 'test@gmail.com','123456','12, Jalan Chowkit, KL','7, Taman Perdana, PJ','0111111111');
+
+INSERT INTO vehicle (dtype,brand, model, vehicle_Year, price, doors)
+VALUES ('Car','Honda','City Hatchback',2021,60_000,4);
