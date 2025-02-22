e9d24e35f8a61fcfe0836f608153b4a5b5f1b835
Muhammad Ammar Zaqwan Bin Mohd Azli <84664689+ammarzaqwan@users.noreply.github.com>
2025-02-22 13:28:52 +0800

    Update Customer.java

--- AutoMarketplace/src/main/java/com/example/AutoMarketplace/model/Customer.java
+++ AutoMarketplace/src/main/java/com/example/AutoMarketplace/model/Customer.java
@@ -1,4 +1,61 @@
 package com.example.AutoMarketplace.model;
 
-public class Customer {
+import jakarta.persistence.Entity;
+
+@Entity
+public class Customer extends Users {
+
+    private String shippingAddress;
+    private String billingAddress;
+    private String phoneNumber;
+
+    // Constructors
+    public Customer() {
+        super();
+    }
+
+    public Customer(String name, String email, String password, String shippingAddress, String billingAddress, String phoneNumber) {
+        super(name, email, password);
+        this.shippingAddress = shippingAddress;
+        this.billingAddress = billingAddress;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getters and Setters
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(String shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public String getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(String billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Override toString() method for better readability
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", shippingAddress='" + shippingAddress + '\'' +
+                ", billingAddress='" + billingAddress + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
