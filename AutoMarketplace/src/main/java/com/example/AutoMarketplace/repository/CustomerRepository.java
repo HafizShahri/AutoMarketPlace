f127c3c5514ae560d5d804849a41fc5bd9c923ed
Azmir <156209190+Azmir08@users.noreply.github.com>
2025-02-22 15:12:00 +0800

    CustomerRepository update
    -add connection to database and create table based on model class

--- AutoMarketplace/src/main/java/com/example/AutoMarketplace/repository/CustomerRepository.java
+++ AutoMarketplace/src/main/java/com/example/AutoMarketplace/repository/CustomerRepository.java
@@ -1,7 +1,9 @@
 package com.example.AutoMarketplace.repository;
 
+import com.example.AutoMarketplace.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
