47c49968f99cf5a0b2824f6087da3c0f3041634e
HafizShahri <hafizshahri97@gmail.com>
2025-02-22 15:06:06 +0800

    modify purchase repo and rename user to users

--- AutoMarketplace/src/main/java/com/example/AutoMarketplace/controller/Purchase.java
+++
@@ -1,9 +0,0 @@
-package com.example.AutoMarketplace.controller;
-
-public class Purchase {
-    private String purchaseId;
-    private String carId;
-    private String customerId;
-    private double price;
-    private String status;
-}
--- AutoMarketplace/src/main/java/com/example/AutoMarketplace/repository/PurchaseRepository.java
+++ AutoMarketplace/src/main/java/com/example/AutoMarketplace/repository/PurchaseRepository.java
@@ -1,23 +1,8 @@
 package com.example.AutoMarketplace.repository;
 
-import com.example.AutoMarketplace.model.Purchase;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Repository
-public class PurchaseRepository {
-    private List<Purchase> purchases = new ArrayList<>();
-
-    public void save(Purchase purchase) {
-        purchases.add(purchase);
-    }
-
-    public List<Purchase> findByCustomerId(String customerId) {
-        return purchases.stream()
-                .filter(p -> p.getId().equals(customerId))
-                .collect(Collectors.toList());
-    }
+public interface PurchaseRepository {
 }
