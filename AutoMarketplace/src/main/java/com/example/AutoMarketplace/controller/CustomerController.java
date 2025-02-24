a444d170d891ee673725dcab6f5b6d979011c791
Azmir <156209190+Azmir08@users.noreply.github.com>
2025-02-24 19:59:31 +0800

    update Controller package
    -add CRUD operation

--- AutoMarketplace/src/main/java/com/example/AutoMarketplace/controller/CustomerController.java
+++ AutoMarketplace/src/main/java/com/example/AutoMarketplace/controller/CustomerController.java
@@ -1,7 +1,72 @@
 package com.example.AutoMarketplace.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.AutoMarketplace.model.Car;
+import com.example.AutoMarketplace.model.Customer;
+import com.example.AutoMarketplace.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CustomerController {
+
+    @Autowired
+    CustomerService service;
+
+    @GetMapping("/customers")
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        return new ResponseEntity<>(service.getAllCustomers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable long id) {
+        Customer customer = service.getCustomerById(id);
+
+        if (customer != null) {
+            return new ResponseEntity<>(customer, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/customer")
+    public ResponseEntity<?> addCustomer(@RequestBody Customer customer) {
+
+        try {
+            Customer customer1 = service.addCustomer(customer);
+            return new ResponseEntity<>(customer1, HttpStatus.CREATED);
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/customer/{id}")
+    public ResponseEntity<String> updateCustomer(@PathVariable long id, @RequestBody Customer customer) {
+
+        Customer customer1 = null;
+        customer1 = service.updateCustomer(id,customer);
+
+        if (customer1 != null) {
+            return new ResponseEntity<>("Updated", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/customer/{id}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable long id) {
+
+        Customer customer1 = service.getCustomerById(id);
+        if (customer1 != null) {
+            service.deleteCustomer(id);
+            return new ResponseEntity<>("Deleted", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Product not found", HttpStatus.OK);
+        }
+    }
+
 }
