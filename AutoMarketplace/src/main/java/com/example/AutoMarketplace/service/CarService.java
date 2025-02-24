3de1ce91822fb668ae8676b9d7aa5ee3ac5456d9
Azmir <156209190+Azmir08@users.noreply.github.com>
2025-02-24 20:03:20 +0800

    update service package
    -add CRUD operation on car,customer,motorcycle class

--- AutoMarketplace/src/main/java/com/example/AutoMarketplace/service/CarService.java
+++ AutoMarketplace/src/main/java/com/example/AutoMarketplace/service/CarService.java
@@ -1,4 +1,36 @@
 package com.example.AutoMarketplace.service;
 
+import com.example.AutoMarketplace.model.Car;
+import com.example.AutoMarketplace.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CarService {
+    @Autowired
+    private CarRepository repo;
+
+    public List<Car> getAllCars() {
+        return repo.findAll();
+    }
+
+    public Car getCarById(long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Car addCar(Car car) {
+        System.out.println("service class");
+        System.out.println(car);
+        return repo.save(car);
+    }
+
+    public Car updateCar(long id, Car car) {
+        return repo.save(car);
+    }
+
+    public void deleteCar(long id) {
+        repo.deleteById(id);
+    }
 }
--- AutoMarketplace/src/main/java/com/example/AutoMarketplace/service/MotorcycleService.java
+++ AutoMarketplace/src/main/java/com/example/AutoMarketplace/service/MotorcycleService.java
@@ -1,4 +1,34 @@
 package com.example.AutoMarketplace.service;
 
+import com.example.AutoMarketplace.model.Motorcycle;
+import com.example.AutoMarketplace.repository.MotorcycleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MotorcycleService {
+    @Autowired
+    private MotorcycleRepository repo;
+
+    public List<Motorcycle> getAllMotorcycles() {
+        return repo.findAll();
+    }
+
+    public Motorcycle getMotorcycleById(long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Motorcycle addMotorcycle(Motorcycle motorcycle) {
+        return repo.save(motorcycle);
+    }
+
+    public Motorcycle updateMotorcycle(long id, Motorcycle motorcycle) {
+        return repo.save(motorcycle);
+    }
+
+    public void deleteMotorcycle(long id) {
+        repo.deleteById(id);
+    }
 }
