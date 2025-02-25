cad73ceaa8118e6d1dd409feeb46a997497ed93b
Azmir <156209190+Azmir08@users.noreply.github.com>
2025-02-25 20:33:11 +0800

    Update dummy data sql

--- AutoMarketplace/src/main/resources/data.sql
+++ AutoMarketplace/src/main/resources/data.sql
@@ -2,8 +2,8 @@ INSERT INTO customer (name, email, password, shipping_Address, billing_Address, 
 VALUES ('admin', 'admin@gmail.com','123456','12, Jalan Chowkit, KL','7, Taman Perdana, PJ','0111111111'),
        ('test', 'test@gmail.com','123456','12, Jalan Chowkit, KL','7, Taman Perdana, PJ','0111111111');
 --
-INSERT INTO car (brand, name, vehicle_Year, price, doors)
-VALUES ('Honda','City Hatchback',2021,60_000,4);
+INSERT INTO car (brand, name, type, vehicle_Year, price, doors)
+VALUES ('Honda','City Hatchback','car',2021,60_000,4);
 
-INSERT INTO motorcycle (brand, name, vehicle_Year, price)
-VALUES ('Yamaha','LC135 V8',2024,7_999);
+INSERT INTO motorcycle (brand, name, type, vehicle_Year, price)
+VALUES ('Yamaha','LC135 V8','motorcycle',2024,7_999);
