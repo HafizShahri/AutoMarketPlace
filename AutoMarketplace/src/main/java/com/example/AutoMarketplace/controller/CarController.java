f137d41f2a6d047d9509716328db1ee23aabe227
Azmir <156209190+Azmir08@users.noreply.github.com>
2025-02-26 22:54:26 +0800

    Update Car Controller
    -update validation method for car

--- AutoMarketplace/src/main/java/com/example/AutoMarketplace/controller/CarController.java
+++ AutoMarketplace/src/main/java/com/example/AutoMarketplace/controller/CarController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -20,14 +19,21 @@ public class CarController {
         return new ResponseEntity<>(service.getAllCars(), HttpStatus.OK);
     }
 
-    @GetMapping("/car/{id}")
-    public ResponseEntity<Car> getCar(@PathVariable long id) {
-        Car car = service.getCarById(id);
+    @GetMapping("/car/")
+    public ResponseEntity<?> getCar() {
+        return new ResponseEntity<>("Product not found", HttpStatus.OK);
+    }
 
-        if (car != null) {
-            return new ResponseEntity<>(car, HttpStatus.OK);
+    @GetMapping("/car/{id}")
+    public ResponseEntity<?> getCar(@PathVariable long id) {
+        //Car car = service.getCarById(id);
+        Car car1 = service.getCarById(id);
+        System.out.println(car1);
+        if (car1 != null) {
+            Car car2 = service.getCarById(id);
+            return new ResponseEntity<>(car2, HttpStatus.OK);
         } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("Product not found", HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -43,17 +49,29 @@ public class CarController {
         }
     }
 
+    @PutMapping("/car/")
+    public ResponseEntity<String> updateProduct() {
+
+        return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
+    }
+
     @PutMapping("/car/{id}")
     public ResponseEntity<String> updateProduct(@PathVariable long id, @RequestBody Car car) {
 
-        Car car1 = null;
-        car1 = service.updateCar(id,car);
-
+        Car car1 = service.getCarById(id);
+        System.out.println(car1);
         if (car1 != null) {
+            service.updateCar(id, car);
             return new ResponseEntity<>("Updated", HttpStatus.OK);
         } else {
             return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @DeleteMapping("/car/")
+    public ResponseEntity<String> deleteProduct() {
+
+        return new ResponseEntity<>("Product not found", HttpStatus.BAD_REQUEST);
     }
 
     @DeleteMapping("/car/{id}")
@@ -64,7 +82,7 @@ public class CarController {
             service.deleteCar(id);
             return new ResponseEntity<>("Deleted", HttpStatus.OK);
         } else {
-            return new ResponseEntity<>("Product not found", HttpStatus.OK);
+            return new ResponseEntity<>("Product not found", HttpStatus.BAD_REQUEST);
         }
     }
 }
